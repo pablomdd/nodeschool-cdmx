76b8ad20208775d0280396a940316dc3a5fec8c9
pablomdd <pablo.mdd.007@gmail.com>
2020-02-10 09:54:53 -0600

    add javascripting exercises 1 - 16

---
+++ javascripting/revising-strings.js
@@ -0,0 +1,5 @@
+let pizza = 'pizza is alright'
+// console.log(pizza)
+
+pizza = pizza.replace('alright','wonderful')
+console.log(pizza)
---
+++ javascripting/string-length.js
@@ -0,0 +1,5 @@
+const example = 'example string'
+// var numero = 55
+
+console.log(example.length)
+// console.log(numero)
